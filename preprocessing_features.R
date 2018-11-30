879f6a300dc3255030e8339f697163675281fb1a
Pau Rodriguez <rodriguez.pau@gmail.com>
2018-11-30 01:52:32 +0100

    preprocessing final

--- preprocessing_features.R
+++ preprocessing_features.R
@@ -30,5 +30,54 @@ features.time.window <- function(df, w=2){
     dff <- cbind(dff,dfnew)
   }  
   dff <- cbind(dff,df[,(ncol(df)-1):ncol(df)])
-  return(dff)
+  return(dff[w:nrow(dff),])
 }
+
+features.prediction <- function(df, d=2){
+  # the data frame must be of the form
+  # names = c('time','x1','x2','x3','error','group')
+  i = d
+  # construct a col vector with all y from future d positions
+  futurey <- rep(0,nrow(df)-d+1)
+  for (k in 1:(nrow(df)-d+1)){
+    futurey[k] <- df$y[k+d]
+  }
+  # remove last d rows
+  df2 <- df[1:(nrow(df)-d+1),]
+  # add future column
+  df2$futurey <- futurey
+  return(df2)
+}
+
+
+data.preparation <- function( dirpath, w, d, dsw, maw){
+  
+  df <- read_files(dirpath)
+  print(dim(df))
+  
+  # downsampling
+  df.downsampled <- down.sample.avg(df,dsw)
+  
+  # ma on error 
+  df.downsampled$y <- smoothing(df.downsampled$error, w=maw, coefs=c() )
+  
+  # apply w window
+  df.downsampled <- features.time.window(df.downsampled,w)  
+  
+  # apply d future pred -> new column future.y
+  df.downsampled <- features.prediction(df.downsampled,d)
+  print(dim(df.downsampled))
+  
+  # save data
+  filename <- paste("csv_small",w,d,dsw,maw,sep="_")
+  filenamer <- paste(filename,".Rdata",sep="")
+  save(df.downsampled, file = filenamer)
+  
+  filenamecsv <- paste(filename,".csv",sep="")
+  write.csv(df.downsampled, file = filenamecsv)
+  
+  
+  
+}
+
+
