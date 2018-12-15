e761af580bba73bbeea57ac03e22e154237b0841
Alexander Parunov <alexander.parunov@gmail.com>
2018-12-15 20:54:10 +0100

    added SVM implementation

--- naive_bayes.R
+++ naive_bayes.R
@@ -1,14 +1,17 @@
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-load(file = 'data/preprocessed/original_features_w10_d10.Rdata')
-df <- df3
-rm(df3)
+load(file = 'data/preprocessed/real_original_features_w10_d10.Rdata')
+load(file = 'data/preprocessed/disc_original_features_w10_d10.Rdata')
 
-library(e1071)
-head(df)
+features <- c("x1","x2","x3","sx1","sx2","sx3")
 
-runNaiveBayes <- function(df, features) {
+trainModel <- function(df, features, algorithm) {
+  if(algorithm == "svm") {
+    df <- df[1:20000,]
+  }
+
+  library(e1071)
   features <- c(features, "futurey","real.futurey")
   df <- df[features]
   test_size <- floor(0.75*nrow(df))
@@ -17,18 +20,25 @@ runNaiveBayes <- function(df, features) {
   
   train <- df[train_ind,]
   test <- df[-train_ind,]
+  model <- NULL
   
-  nb <- naiveBayes(futurey ~ ., data = train)
-  pred <- predict(nb, test)
+  if(algorithm == "naiveBayes") {
+    model <- naiveBayes(futurey ~ ., data=train)
+  } else if(algorithm == "svm") {
+    model <- svm(futurey ~ ., data = train)
+  } else {
+    print("algorithm should be naiveBayes or svm")
+    return(NULL)
+  }
+  
+  pred <- predict(model, test)
   pred <- as.numeric(as.character(pred))
   
   real <- test$real.futurey
   mape <- sum(abs(real-pred)/pred)/length(pred)
   
-  return(list(model=nb, mape=mape))
+  return(list(model=model, mape=mape))
 }
 
-features <- c("x1","x2","x3","sx1","sx2","sx3")
-
-mod_res <- runNaiveBayes(df, features)
-
+nb_res <- trainModel(df_final_disc, features, "naiveBayes")
+svm_res <- trainModel(df_final_real, features, "svm")
